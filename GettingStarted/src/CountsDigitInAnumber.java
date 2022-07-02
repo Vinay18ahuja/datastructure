6aef767894fb0f9916794aea32bd3310c21c03df
Vinay18ahuja <vinay18ahuja@gmail.com>
2022-07-02 16:01:54 +0530

    commit2

--- GettingStarted/src/CountsDigitInAnumber.java
+++ GettingStarted/src/CountsDigitInAnumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CountsDiginInAnumber {
+public class CountsDigitInAnumber {
     public static void main(String[] args) {
         // write your code here
         Scanner scn=new Scanner(System.in);
